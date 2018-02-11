8dff7fdad4f5c945752108a465595b64641a27d6
Scrotuss <36317639+Scrotuss@users.noreply.github.com>
2018-02-11 20:34:35 +0100

    Adding a line for git test

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * adding this line for see how git works
  */
 package javaapplication1; 
 
